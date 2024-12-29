1f9073945b24bf693f6b2c7898c7477ed7b7648d
ToukirKhondokar <toukirk779@gmail.com>
2024-12-29 19:32:13 +0600

    Add project files.

---
+++ CreditCard.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment_2
+{
+    public class CreditCard
+    {
+        public const decimal MAX_TRANSACTION_LIMIT = 500000m;
+        public const decimal PER_DAY_TRANSACTION_LIMIT = 100000m;
+        public const decimal PER_TRANSACTION_LIMIT = 20000m;
+
+        private decimal _dailyTransactedAmount = 0m;
+        private decimal _totalTransactedAmount = 0m;
+
+
+        public void Withdraw(decimal amount)
+        {
+            bool isSuccessful = false;
+
+            if (amount <= 0)
+            {
+                throw new ArgumentException("amount can not be zero or negetive");
+            }
+
+            if (amount > PER_TRANSACTION_LIMIT)
+            {
+                throw new ArgumentException("per transcation limit exceed");
+            }
+
+            if (ValidatePerDayTransactionLimit(amount))
+            {
+                throw new ArgumentException("per day transaction limit exceed");
+            }
+
+            if (ValidateMaxTransactionLimit(amount))
+            {
+                throw new ArgumentException("max transaction limit exceed");
+            }
+
+            _dailyTransactedAmount += amount;
+            _totalTransactedAmount += amount;
+            isSuccessful = true;
+            Console.WriteLine($"Transaction Sucessful");
+            //return isSuccessful;
+        }
+
+        public void PayBill(decimal amount)
+        {
+            //bool isSuccessful = false;
+
+            if (amount <= 0)
+            {
+                throw new ArgumentException("amount can not be zero or negetive");
+            }
+
+            if (ValidateMaxTransactionLimit(amount))
+            {
+                throw new ArgumentException("max transaction limit exceed");
+            }
+
+            _totalTransactedAmount += amount;
+            //return isSuccessful;
+            Console.WriteLine($"Bill Patment Sucessful");
+
+        }
+
+
+        private bool ValidatePerDayTransactionLimit(decimal amount) => (_dailyTransactedAmount + amount) > PER_DAY_TRANSACTION_LIMIT;
+        private bool ValidateMaxTransactionLimit(decimal amount) => (_dailyTransactedAmount + amount) > PER_DAY_TRANSACTION_LIMIT;
+
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assingment_2
+{
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreditCard credit_card_1 = new CreditCard();
+                credit_card_1.Withdraw(500);
+                credit_card_1.PayBill(1000000);
+
+
+                CreditCard credit_card_2 = new CreditCard();
+                credit_card_1.Withdraw(22000);
+                credit_card_1.PayBill(100000);
+
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"{ex.Message}");    
+            
+            }
+
+        }
+    }
+}
